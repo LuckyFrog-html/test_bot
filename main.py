380c353680593b191a47b6487b32e2a97ffc6665
LuckyFrog-html <bogvanya.04@mail.ru>
2022-11-09 17:43:42 +0300

    test

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import telebot
+# import telebot
+#
+# bot = telebot.TeleBot("5660357300:AAEagrDoMcNNF9d3-zS2zQcXG4fJU6Tmaas")
+#
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
+#
+#
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+#
+#
+# bot.infinity_polling()
 
-bot = telebot.TeleBot("5660357300:AAEagrDoMcNNF9d3-zS2zQcXG4fJU6Tmaas")
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+print("workin")
