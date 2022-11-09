3267f05cdee44e2a9d9b7bca957e55aa29570973
LuckyFrog-html <bogvanya.04@mail.ru>
2022-11-09 20:31:21 +0300

    init

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
 import telebot
-from pm2py import PM2
-
 
 bot = telebot.TeleBot("5660357300:AAEagrDoMcNNF9d3-zS2zQcXG4fJU6Tmaas")
 
 print('gelo')
 
-
-pm2 = PM2()
-
-pm2.delete("all")  # delete all processes
-
-pm2.start("tests/test_script.py", ["-f"])
-pm2.start("tests/test_script2.py", ["--name", "erkinbaba", "-f"])
-
-for process in pm2.list():
-    if process.status == "online":
-        print("PROCESS ONLINE: " + process.name)
-    else:
-        print(f"PROCESS {process.status.upper()}: " + process.name)
-
-
-def logger(process):
-    print(
-        f"{'>' if process['type'] == 'out' else '<'} | {process['app_name']} | {process['message']}")
-
-
-pm2.logs(logger)  # sync method, will work until complete
 
 
 @bot.message_handler(commands=['start', 'help'])
